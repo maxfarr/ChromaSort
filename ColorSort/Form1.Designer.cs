3cb957abece0bad611b16c8dd632a318d97d8bd9
Max Farr <maxfarr@stanford.edu>
2017-10-31 17:08:49 -0700

    canvas works

--- ColorSort/Form1.Designer.cs
+++ ColorSort/Form1.Designer.cs
@@ -33,8 +33,10 @@
             this.sortTick = new System.Windows.Forms.Timer(this.components);
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.sortsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.randomHueUnlimitedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sortsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.bubbleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bubbleWorker = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -42,63 +44,84 @@
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 44);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(984, 737);
+            this.pictureBox1.Size = new System.Drawing.Size(335, 211);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // sortTick
             // 
             this.sortTick.Enabled = true;
-            this.sortTick.Interval = 200;
+            this.sortTick.Interval = 20;
             this.sortTick.Tick += new System.EventHandler(this.sortTick_Tick);
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sortsToolStripMenuItem,
             this.sortsToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(335, 44);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // sortsToolStripMenuItem
             // 
+            this.sortsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.randomHueUnlimitedToolStripMenuItem});
             this.sortsToolStripMenuItem.Name = "sortsToolStripMenuItem";
-            this.sortsToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.sortsToolStripMenuItem.Size = new System.Drawing.Size(115, 36);
             this.sortsToolStripMenuItem.Text = "Initialize";
             this.sortsToolStripMenuItem.Click += new System.EventHandler(this.sortsToolStripMenuItem_Click);
+            // 
+            // randomHueUnlimitedToolStripMenuItem
+            // 
+            this.randomHueUnlimitedToolStripMenuItem.Name = "randomHueUnlimitedToolStripMenuItem";
+            this.randomHueUnlimitedToolStripMenuItem.Size = new System.Drawing.Size(379, 38);
+            this.randomHueUnlimitedToolStripMenuItem.Text = "Random Hue (Unlimited)";
+            this.randomHueUnlimitedToolStripMenuItem.Click += new System.EventHandler(this.randomHueUnlimitedToolStripMenuItem_Click);
             // 
             // sortsToolStripMenuItem1
             // 
             this.sortsToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.bubbleToolStripMenuItem});
             this.sortsToolStripMenuItem1.Name = "sortsToolStripMenuItem1";
-            this.sortsToolStripMenuItem1.Size = new System.Drawing.Size(45, 20);
-            this.sortsToolStripMenuItem1.Text = "Sorts";
+            this.sortsToolStripMenuItem1.Size = new System.Drawing.Size(70, 36);
+            this.sortsToolStripMenuItem1.Text = "Sort";
             // 
             // bubbleToolStripMenuItem
             // 
             this.bubbleToolStripMenuItem.Name = "bubbleToolStripMenuItem";
-            this.bubbleToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bubbleToolStripMenuItem.Size = new System.Drawing.Size(189, 38);
             this.bubbleToolStripMenuItem.Text = "Bubble";
             this.bubbleToolStripMenuItem.Click += new System.EventHandler(this.bubbleToolStripMenuItem_Click);
             // 
+            // bubbleWorker
+            // 
+            this.bubbleWorker.WorkerReportsProgress = true;
+            this.bubbleWorker.WorkerSupportsCancellation = true;
+            this.bubbleWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bubbleWorker_DoWork);
+            this.bubbleWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bubbleWorker_ProgressChanged);
+            this.bubbleWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bubbleWorker_RunWorkerCompleted);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.ClientSize = new System.Drawing.Size(335, 255);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
-            this.MaximizeBox = false;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "ColorSort";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -115,6 +138,8 @@
         private System.Windows.Forms.ToolStripMenuItem sortsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sortsToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem bubbleToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker bubbleWorker;
+        private System.Windows.Forms.ToolStripMenuItem randomHueUnlimitedToolStripMenuItem;
     }
 }
 
--- ColorSort/Form1.cs
+++ ColorSort/Form1.cs
@@ -3,69 +3,120 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Spectrum;
 using static Spectrum.Color;
+using System.Threading;
 
 namespace ColorSort
 {
+
+    struct Vector2
+    {
+        public int X;
+        public int Y;
+
+        public Vector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
     public partial class Form1 : Form
     {
-        Bitmap image;
+        Bitmap canvas;
+        Bitmap colorImage;
         Graphics gfx;
 
         Random rand;
+
+        Size initialSize;
 
         ColorLine[] lines;
 
         public Form1()
         {
             InitializeComponent();
-
             rand = new Random();
-            image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            gfx = Graphics.FromImage(image);
-            lines = new ColorLine[pictureBox1.Width];
+            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gfx = Graphics.FromImage(canvas);
+            colorImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            initialSize = pictureBox1.Size;
+            lines = new ColorLine[initialSize.Width];
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
 
-            for (int i = 0; i < pictureBox1.Width; i++)
+            for (int i = 0; i < initialSize.Width; i++)
             {
-                lines[i] = new ColorLine(pictureBox1.Height, rand);
-            }
-
-            for (int i = 0; i < pictureBox1.Width; i++)
-            {
-                for (int j = 0; j < pictureBox1.Height; j++)
+                lines[i] = new ColorLine(initialSize.Height, rand);
+                for (int j = 0; j < initialSize.Height; j++)
                 {
                     RGB currentPixel = lines[i].Colors[j].ToRGB();
-                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(255, currentPixel.R, currentPixel.G, currentPixel.B));
+                    colorImage.SetPixel(i, j, System.Drawing.Color.FromArgb(255, currentPixel.R, currentPixel.G, currentPixel.B));
                 }
             }
-            
+
+            gfx.DrawImage(colorImage, new Rectangle(Point.Empty, pictureBox1.Size));
+            pictureBox1.Image = canvas;
         }
 
         private void sortTick_Tick(object sender, EventArgs e)
         {
             //gfx.Clear(BackColor);
             //image.SetPixel();
+            colorImage = generateImage();
+            gfx.DrawImage(colorImage, new Rectangle(Point.Empty, pictureBox1.Size));
+            pictureBox1.Image = canvas;
+        }
 
-            for (int i = 0; i < pictureBox1.Width; i++)
+        void initialize()
+        {
+            for (int i = 0; i < initialSize.Width; i++)
             {
-                for (int j = 0; j < pictureBox1.Height; j++)
+                lines[i] = new ColorLine(initialSize.Height, rand);
+                for (int j = 0; j < initialSize.Height; j++)
                 {
                     RGB currentPixel = lines[i].Colors[j].ToRGB();
-                    image.SetPixel(i, j, System.Drawing.Color.FromArgb(255, currentPixel.R, currentPixel.G, currentPixel.B));
+                    canvas.SetPixel(i, j, System.Drawing.Color.FromArgb(255, currentPixel.R, currentPixel.G, currentPixel.B));
                 }
             }
 
-            pictureBox1.Image = image;
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+            gfx.DrawImage(canvas, new Rectangle(Point.Empty, pictureBox1.Size));
+            pictureBox1.Image = canvas;
         }
 
-        void updateImage()
+        void updateSwap(int[] swap)
         {
-            
+            RGB pixelA = lines[swap[0]].Colors[swap[1]].ToRGB();
+            RGB pixelB = lines[swap[0]].Colors[swap[2]].ToRGB();
+
+            colorImage.SetPixel(swap[0], swap[1], System.Drawing.Color.FromArgb(255, pixelB.R, pixelB.G, pixelB.B));
+            colorImage.SetPixel(swap[0], swap[2], System.Drawing.Color.FromArgb(255, pixelA.R, pixelA.G, pixelA.B));
+
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+            gfx.DrawImage(colorImage, new Rectangle(Point.Empty, pictureBox1.Size));
+            pictureBox1.Image = canvas;
+        }
+
+        Bitmap generateImage()
+        {
+            Bitmap bitmap = new Bitmap(initialSize.Width, initialSize.Height);
+
+            for (int i = 0; i < initialSize.Width; i++)
+            {
+                for (int j = 0; j < initialSize.Height; j++)
+                {
+                    RGB currentPixel = lines[i].Colors[j].ToRGB();
+                    bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(255, currentPixel.R, currentPixel.G, currentPixel.B));
+                }
+            }
+
+            return bitmap;
         }
 
         void bubbleSort()
@@ -76,24 +127,25 @@ namespace ColorSort
             {
                 int sortedCount = 0;
 
-                for (int i = 0; i < pictureBox1.Width; i++)
+                for (int i = 0; i < lines.Length; i++)
                 {
                     if (!lines[i].Sorted)
                     {
                         lines[i].Sorted = true;
 
-                        for (int j = 1; j < pictureBox1.Height; j++)
+                        for (int j = 1; j < lines[i].Colors.Length; j++)
                         {
                             if (lines[i].Colors[j].H < lines[i].Colors[j - 1].H)
                             {
                                 lines[i].Sorted = false;
+
                                 HSV temp = lines[i].Colors[j];
                                 lines[i].Colors[j] = lines[i].Colors[j - 1];
                                 lines[i].Colors[j - 1] = temp;
+
+                                int[] swap = new int[3] { i, j, j - 1 };
                             }
                         }
-
-                        updateImage();
                     }
                     else
                     {
@@ -101,7 +153,7 @@ namespace ColorSort
                     }
                 }
 
-                if (sortedCount == pictureBox1.Width)
+                if (sortedCount == initialSize.Width)
                 {
                     sorted = true;
                 }
@@ -113,13 +165,51 @@ namespace ColorSort
 
         }
 
-        private async void bubbleToolStripMenuItem_Click(object sender, EventArgs e)
+        private void bubbleToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            await Task.Run(() =>
+            bubbleSort();
+        }
+
+        private void bubbleWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Error != null)
             {
-                bubbleSort();
-            });
-            //bubbleSort();
+                string error = "Error: " + e.Error.Message;
+            }
+            generateImage();
+        }
+
+        private void bubbleWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            updateSwap((int[])e.UserState);
+            //updateImage();
+        }
+
+        private void bubbleWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            bubbleSort();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gfx = Graphics.FromImage(canvas);
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+            colorImage = generateImage();
+            gfx.DrawImage(colorImage, new Rectangle(Point.Empty, pictureBox1.Size));
+            pictureBox1.Image = canvas;
+        }
+
+        private void randomHueUnlimitedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            initialize();
         }
     }
 }
